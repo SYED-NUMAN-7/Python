af470844be34b0e28cc13bb482df2f2fa6921d5d
SYED NUMAN <nomanull789@gmail.com>
2025-11-13 22:13:35 +0530

    Create possibilities.py

---
+++ possibilities.py
@@ -0,0 +1,8 @@
+FOR POSSIBLE OUTCOMES OF ELEMENTS IN A LIST WHERE I + J + K != N 
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    print(list([i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1)  if i+j+k !=n))
